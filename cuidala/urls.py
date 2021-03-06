85970394df71ee3e98d5a94aff1b083f590b9e43
Andrescortes23 <andresccortes23@icloud.com>
2021-03-06 10:26:20 -0500

    Correcting an import

--- cuidala/urls.py
+++ cuidala/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Database import views
+from backend import views
 from rest_framework.routers import DefaultRouter
 from backend.views import *
 

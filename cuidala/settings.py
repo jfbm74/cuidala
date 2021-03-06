7fb8c7430555cbe87ba042ccff55d8c3321e3656
Andrescortes23 <andresccortes23@icloud.com>
2021-03-06 09:57:32 -0500

    Congurate settings to set the Database

--- cuidala/settings.py
+++ cuidala/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'cuidala.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CuidaLA',
+        'USER': 'cuidala',
+        'PASSWORD': 'Cuidala2021!',
     }
 }
 

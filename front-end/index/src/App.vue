2b23b9929e8e5564651a1958f4a231327079f73b
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-03-07 11:47:58 -0500

    directorio del proyecto de vue, visualización de la pagina de inicio

---
+++ front-end/index/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="app">
+        <Navegacion/>
+        <SliderArea/>
+        <NavService/>
+        <Section/>
+        <Contacto/>
+        <Footer/>
+    </div>
+</template>
+
+<script>
+import Navegacion from '@/components/Navegacion.vue'
+import Section from '@/components/Section.vue'
+import NavService from '@/components/NavService.vue'
+import SliderArea from '@/components/SliderArea.vue'
+import Contacto from '@/components/Contacto.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navegacion,
+    SliderArea,
+    NavService,
+    Section,
+    Contacto,
+    Footer
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ front-end/index/src/components/Navegacion.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <header>
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Sobre Nosotros</a></li>
+        <li><a href="#">Cuidadores</a></li>
+        <li><a href="#">Contacto</a></li>
+        <li><a href="registro.html">Reg&iacute;strate</a></li>
+        <li><a href="#">Login</a></li>
+      </ul>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navegacion',
+}
+</script>
+
+
+<style scoped>
+ul {
+  margin: 10px 20px 10px 20px;
+  float: right;
+}
+ul li {
+  list-style: none;
+  font-family: Arial;
+  float: left;
+}
+ul li a {
+  color: grey;
+  text-decoration: none;
+  padding: 10px;
+}
+ul li a:hover {
+  background-color: #AAAAAA;
+  border-radius: 10px;
+}
+</style>
